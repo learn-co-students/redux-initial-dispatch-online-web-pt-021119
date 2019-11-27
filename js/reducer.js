dab51607bcc98b97dd195fee74e05d2d661c4fdc
github username <github email address>
2019-11-26 17:48:23 -0700

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,17 @@
-let state = {count: 0};
 
-function changeState(state, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+let state;
+
+function changeState(state = { count: 0 }, action) {
+ 
+  switch (action.type) {
+
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 }
+
+    default:
+      return state;
   }
+}
 
 function dispatch(action){
     state = changeState(state, action)
@@ -17,3 +21,5 @@ function dispatch(action){
 function render(){
     document.body.textContent = state.count
 }
+
+dispatch({ type: '@@INIT' })
